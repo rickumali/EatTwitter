91fdf769ec4522329db7220db10f0bf3328d7158
Rick Umali <rickumali@gmail.com>
2011-11-21 19:08:58 -0500

    This is a sample environment file.
    This is read by the class for the username/password that will
    make the Twitter connection.

---
+++ back/environment-template.php
@@ -0,0 +1,9 @@
+<?php
+# NOTE: This is a template for the environment.php file. This file
+# must be changed to contain the correct username/password of a Twitter
+# account that will actually maintain the connection. 
+# NOTE: I recommend copying this file to a file named environment-ENV.php,
+# where ENV is either DEV, STAGE, or PROD. Then create a soft link (using
+# the ln(1) command) to this file, named environment.php.
+define("USERNAME","username");
+define("PASSWORD","password");
