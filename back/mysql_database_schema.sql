7bdc7b0da176470d7b82092c78dd1f97f3ffb993
Rick Umali <rickumali@gmail.com>
2011-11-26 20:08:59 -0500

    Adds parent_id to the food_tags table.
    This is to allow for hierarchical tags. I'm going to want this. The
    first pass will be with two-level hierarchy.

--- back/mysql_database_schema.sql
+++ back/mysql_database_schema.sql
@@ -52,6 +52,7 @@ CREATE TABLE IF NOT EXISTS `tweet_tags` (
 -- Introduced for Food tags (i.e. the vocabulary)
 -- This contains the food tags
 CREATE TABLE IF NOT EXISTS `food_tags` (
+  `parent_id` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'This is the id of the PARENT food_tag, if there is one. If there isn\'t one, it\'s zero',
   `food_tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'This is the id for the food tag (vocabulary)',
   `tag` varchar(100) NOT NULL COMMENT 'This is the food tag itself.',
   `level` int(1) NOT NULL COMMENT 'This is the \'level\' of the food tag. The lower the number, the more specific the tag is. In general, all tags will be level 1, BUT some tags will be level 2, to represent the \'groupings\' of the tags.',
