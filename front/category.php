1db7db4da242bd39918485902c5001d638cf875f
Rick Umali <rickumali@gmail.com>
2011-12-03 15:48:15 -0500

    Modified code to use 'prepared statements'.
    This should prevent any SQL injection from my use of the GET
    parameter.

--- front/category.php
+++ front/category.php
@@ -4,14 +4,20 @@ require_once('./140dev_config.php');
 require_once('./db_lib.php');
 $oDB = new db;
 $view=$_GET['view'];
-$result = $oDB->select("select food_tags.food_tag_id as viewid from food_tags where tag = \"$view\"");
-$row = mysqli_fetch_assoc($result);
-if (mysqli_num_rows($result) == 0) {
+$pS = $oDB->dbh->prepare("select food_tags.food_tag_id as viewid from food_tags where tag = ?");
+$pS->bind_param('s',$view);
+$pS->execute();
+$pS->bind_result($view_id);
+
+$count = 0;
+while ($pS->fetch()) {
+  $count++;
+}
+if ($count == 0) {
   ob_end_clean();
   header('Location: /eattwitter/');
   exit();
 }
-$view_id = $row['viewid'];
 $result = $oDB->select("select food_tags.tag as tag,count(food_tags.tag) as cnt from tweets_food_tags,food_tags,tweets where tweets.tweet_id = tweets_food_tags.tweet_id and food_tags.food_tag_id = tweets_food_tags.food_tag_id and food_tags.parent_id = $view_id and food_tags.level = 1 group by food_tags.tag order by cnt desc");
 if (mysqli_num_rows($result) == 0) {
   ob_end_clean();
--- front/food_tag.php
+++ front/food_tag.php
@@ -4,14 +4,20 @@ require_once('./140dev_config.php');
 require_once('./db_lib.php');
 $oDB = new db;
 $view=$_GET['view'];
-$result = $oDB->select("select food_tags.food_tag_id as viewid from food_tags where tag = \"$view\"");
-$row = mysqli_fetch_assoc($result);
-if (mysqli_num_rows($result) == 0) {
+$pS = $oDB->dbh->prepare("select food_tags.food_tag_id as viewid from food_tags where tag = ?");
+$pS->bind_param('s',$view);
+$pS->execute();
+$pS->bind_result($view_id);
+
+$count = 0;
+while ($pS->fetch()) {
+  $count++;
+}
+if ($count == 0) {
   ob_end_clean();
   header('Location: /eattwitter/');
   exit();
 }
-$view_id = $row['viewid'];
 $result = $oDB->select("select profile_image_url, created_at, screen_name, name, tweet_text, tweets.tweet_id from tweets, tweets_food_tags where tweets_food_tags.food_tag_id = $view_id and tweets_food_tags.tweet_id = tweets.tweet_id");
 if (mysqli_num_rows($result) == 0) {
   ob_end_clean();
