5e5d0524ba6b453c0dcef637e60676fc8bed943f
Rick Umali <rickumali@gmail.com>
2011-12-11 19:36:02 -0500

    Revamped the files to reflect new hosting.
    Instead of "/eattwitter", the files will be hosted off of "/". I
    update all the files to reflect this. This is BRITTLE, I know, and
    the next version will be in some kind of framework (Zend?) or Drupal
    to avoid this kind of hassle.

--- front/category.php
+++ front/category.php
@@ -16,13 +16,13 @@ while ($pS->fetch()) {
 }
 if ($count == 0) {
   ob_end_clean();
-  header('Location: /eattwitter/');
+  header('Location: /');
   exit();
 }
 $result = $oDB->select("select food_tags.tag as tag,count(food_tags.tag) as cnt from tweets_food_tags,food_tags,tweets where tweets.tweet_id = tweets_food_tags.tweet_id and food_tags.food_tag_id = tweets_food_tags.food_tag_id and food_tags.parent_id = $view_id and food_tags.level = 1 group by food_tags.tag order by cnt desc");
 if (mysqli_num_rows($result) == 0) {
   ob_end_clean();
-  header('Location: /eattwitter/thankyou.html');
+  header('Location: /thankyou.html');
   exit();
 }
 ?>
@@ -44,7 +44,7 @@ print "<h2>Food Group: $food_group[$view]</h2>\n";
 <?php
 $pie_chart_data = '';
 while($row = mysqli_fetch_assoc($result)) {
-  print "<tr>\n<td><a href=\"/eattwitter/tag/$row[tag]\">$row[tag]</a></td>\n<td>$row[cnt]</td></tr>\n";
+  print "<tr>\n<td><a href=\"/tag/$row[tag]\">$row[tag]</a></td>\n<td>$row[cnt]</td></tr>\n";
   $pie_chart_data .= "{ label: \"" . $row['tag']  . " \",  data: $row[cnt]},\n";
 }
 ?>
--- front/index.php
+++ front/index.php
@@ -8,7 +8,7 @@ $oDB = new db;
 $result = $oDB->select("select food_tags.tag as tag,count(food_tags.tag) as cnt from tweets_food_tags,food_tags,tweets where tweets.tweet_id = tweets_food_tags.tweet_id and food_tags.food_tag_id = tweets_food_tags.food_tag_id and food_tags.level = 2 group by food_tags.tag order by cnt desc");
 if (mysqli_num_rows($result) == 0) {
   ob_end_clean();
-  header('Location: thankyou.html');
+  header('Location: /thankyou.html');
   exit();
 }
 ?>
